eb7e262a44463ca9f6c60b188ffb90925062cc1b
YudAike <ikeda.yuta298@mail.kyutech.jp>
2025-07-16 16:26:19 +0900

    Add initial files: Amino_name.py, Hello_World.c, accesstoken.docx

---
+++ Amino_name.py
@@ -0,0 +1,25 @@
+amino_acids = [
+    "アラニン (Ala, A)",
+    "アルギニン (Arg, R)",
+    "アスパラギン (Asn, N)",
+    "アスパラギン酸 (Asp, D)",
+    "システイン (Cys, C)",
+    "グルタミン (Gln, Q)",
+    "グルタミン酸 (Glu, E)",
+    "グリシン (Gly, G)",
+    "ヒスチジン (His, H)",
+    "イソロイシン (Ile, I)",
+    "ロイシン (Leu, L)",
+    "リシン (Lys, K)",
+    "メチオニン (Met, M)",
+    "フェニルアラニン (Phe, F)",
+    "プロリン (Pro, P)",
+    "セリン (Ser, S)",
+    "スレオニン (Thr, T)",
+    "トリプトファン (Trp, W)",
+    "チロシン (Tyr, Y)",
+    "バリン (Val, V)"
+]
+
+for aa in amino_acids:
+    print(aa)
---
+++ Hello_World.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
+// This program prints "Hello World!" to the console.
+// It serves as a simple introduction to C programming.
+// To compile this code, use a C compiler like gcc:
+// gcc -o hello_world hello_world.c
+// Then run the executable: ./hello_world
